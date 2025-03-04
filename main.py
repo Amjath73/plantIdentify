1efd249712c7719e970b8cfdc385144a07f04e70
Amjath <147572251+Amjath73@users.noreply.github.com>
2025-03-04 12:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,6 +110,6 @@ async def predict(file: UploadFile = File(...)):
 
 # Run FastAPI
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))  # Railway provides PORT dynamically
+    port = int(os.getenv("PORT", 8000))  # Railway provides PORT dynamically
     print(f"🚀 Starting server on port {port}...")
     uvicorn.run(app, host="0.0.0.0", port=port)
